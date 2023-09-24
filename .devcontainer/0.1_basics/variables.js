690bb1874bea066f38571150d4a4684b38815af7
deepanjali6111 <145878298+deepanjali6111@users.noreply.github.com>
2023-09-24 14:32:58 +0000

    variables

---
+++ .devcontainer/0.1_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 122456
+let accountEmail= "deepanjali@gmail.com"
+var accountPassword = "348757"
+accountCity = "jaipur"
+
+//accountId=2 //not allowed
+console.log(accountId);
+accountEmail ="avbc@gmail.com"
+accountPassword="64565487"
+accountCity="bekjgnj"
+let accountState;
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*prefer not to use var 
+because of block scope and functional scope
+*/
