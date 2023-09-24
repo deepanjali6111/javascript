89dc17717ae51e40449b79f27d5c8239eaae6904
deepanjali6111 <145878298+deepanjali6111@users.noreply.github.com>
2023-09-24 14:54:04 +0000

    datatypes

---
+++ .devcontainer/0.1_basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict" ;//treat all js code as newer version
+//alert(3+3) //we are using nodejs not browser
+// refer to:
+let name="deepa"//string
+let age=20 //integer
+let islogged=true
+console.table([name,age])
