ba12dc8ef4f6564f2ee9f1361dc1ed758f215d91
deepanjali6111 <145878298+deepanjali6111@users.noreply.github.com>
2023-10-01 07:43:07 +0000

    date and time

--- .devcontainer/0.1_basics/comparision.js
+++ .devcontainer/0.1_basics/comparision.js
@@ -18,3 +18,11 @@ console.log(myObj)
 const func  = function(){
     console.log("helloo");
 }
+/****stack and heap */
+/*primitve like string are stored in stack like string and if we pass it valuues to another than its copy is passed and original is not changed*/
+/*in heap non-primitive are stored like object and if value is passed to another than original value is passed and any change in that will lead to change in original one*/
+console.log(Math.random())//give value b/w 0 and 1
+console.log((Math.random()*10)+1)//digit is shifted and to avoid 0 ans we added 1 to it
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random()*(max-min+1))+min)//range is passed max-min and to avoid zero 1 is added and to ensure min value comes we added min
---
+++ .devcontainer/0.1_basics/date and time.js
@@ -0,0 +1,7 @@
+let mydate  =new Date()
+console.log(mydate.toString())
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleString())
+console.log(typeof mydate)//object
+let createddate  =new Date(2023,9,1)//yr,month,date
+console.log(createddate.toDateString())
