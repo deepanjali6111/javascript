4826ca9f91a1686c2a4f95a0e8434760c16bd933
deepanjali6111 <145878298+deepanjali6111@users.noreply.github.com>
2023-10-03 11:30:27 +0000

    funcTwo

--- .devcontainer/0.3_basics/functions.js
+++ .devcontainer/0.3_basics/functions.js
@@ -15,3 +15,32 @@ add(2,4)//passing arguments to function
 add(2,"4")
 const result= add2(2,4)
 console.log("result:",result)
+ const user={
+    name:"yasif",
+    loginid:"acef",
+    mesageWeb:function(){
+        console.log(`${this.name}`)
+        //console.log(this)
+
+    }
+
+
+ }
+ user.mesageWeb()
+
+ user.name="samuel"
+ user.mesageWeb()
+ console.log(this)
+ function aplha(){
+    //console.log(this);
+ }
+ aplha()
+ const beta = (num1,num2) => {
+    return num1+num2
+ }
+ console.log(beta(6,7))
+ const betaOne = (num1,num2) =>  num1+num2
+ console.log(betaOne(6,7))
+ const betaTwo = (num1,num2) =>  ({username:"meral"})
+ console.log(betaTwo(6,7))
+ 
