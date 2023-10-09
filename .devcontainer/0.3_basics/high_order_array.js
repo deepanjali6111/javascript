43b39f0cfc60d0d9b4d7de8ffe2132e60788563e
deepanjali6111 <145878298+deepanjali6111@users.noreply.github.com>
2023-10-09 17:13:20 +0000

    map

--- .devcontainer/0.3_basics/high_order_array.js
+++ .devcontainer/0.3_basics/high_order_array.js
@@ -67,7 +67,34 @@ const beta = [
         codein:"pycharm"
     },
 ]
-beta.forEach( (item) => {
+/*beta.forEach( (item) => {
     console.log(item.codein)
 
+})*/
+/*const value=coding.forEach( (item) => {
+    //console.log(item)
+    return item; //.foreach doesnot return anything its undefined
 })
+console.log(value) */
+const nums=[1,2,3,4,5,6,7,8,9,10]
+/*const result=nums.filter((num)=> num>4)
+console.log(result);*/
+/*const result2=nums.filter((num)=> {
+    return num>4//scope starts with curly braces nedds return statement
+})
+console.log(result2);*/
+/*const newnum=[]
+nums.forEach( (num)=>{
+    if(num>4){
+        newnum.push(num)
+    }
+})
+console.log(newnum)*/
+/*const gamma = nums.map( (num)=>num+10)
+console.log(gamma)*/
+const chaining = nums
+                    .map( (num)=>num+10)
+                    .map((num)=>num+1)
+                    .filter( (num)=>num>=15)
+console.log(chaining)
+
